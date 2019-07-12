ff86817e1192a605cc14221182e05654634a138d
schultz <ambassador.life@gmail.com>
2019-07-12 12:20:00 +0700

    Refactor code

--- python_basics.py
+++ python_basics.py
@@ -1,7 +1,9 @@
-import math
-import string
-import pygame
-import time
+from math import sqrt
+from string import punctuation
+from time import sleep
+
+from pygame import init
+from pygame.mixer import Sound
 
 
 # Waypoint 1: Say Greeting
@@ -13,23 +15,19 @@ def hello(name):
 # Waypoint 2: Pythagorean Theorem
 def calculate_hypotenuse(a, b):
     """Calculate the length of the hypotenuse of a right triangle."""
-    return math.sqrt(a ** 2 + b ** 2)
+    return sqrt(a ** 2 + b ** 2)
 
 
 # Waypoint 3: Test whether all Conditions are True
 def are_all_conditions_true(conditions):
     """Test whether all conditions are True."""
-    if conditions:
-        return False not in conditions
-    return None
+    return None if not conditions else all(conditions)
 
 
 # Waypoint 4: Test whether at least one Condition is True
 def is_a_condition_true(conditions):
     """Test whether at least conditions are True."""
-    if conditions:
-        return True in conditions
-    return None
+    return None if not conditions else any(conditions)
 
 
 # Waypoint 5: Filter List of Integers
@@ -48,18 +46,16 @@ def find_cheapest_hotels(hotel_daily_rates, maximum_daily_rate):
 # Waypoint 7: Calculate Distance between two 2D Points
 def calculate_euclidean_distance_between_2_points(p1, p2):
     """Calculate distance between two 2D points."""
-    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+    return sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
 
 # Waypoint 8: Calculate Distance between several 2D Points
 def calculate_euclidean_distance_between_points(points):
     """Calculate distance between several 2D points."""
-    # Catch exception
     if len(points) < 2:
         raise ValueError('The list MUST contain at least 2 points')
-
-    return sum([math.sqrt((points[x - 1][0] - points[x][0]) ** 2 +
-                          (points[x - 1][1] - points[x][1]) ** 2)
+    return sum([sqrt((points[x - 1][0] - points[x][0]) ** 2 +
+                     (points[x - 1][1] - points[x][1]) ** 2)
                 for x in range(1, len(points))])
 
 
@@ -84,33 +80,27 @@ def capitalize_words(s):
 
     @raise TypeError: if the argument `s` is not a string.
     """
-    # Catch exception
+    if s is None:
+        return None
     if not isinstance(s, str):
         raise TypeError('Not a string')
-
-    if s:
-        return ' '.join(s.split()).title()
-    return None
+    return ' '.join(s.split()).title()
 
 
 # Waypoint 10: Uppercase and Lowercase Words
 def uppercase_lowercase_words(s):
     """Change even word to uppercase and odd word to lowercase."""
-    # Catch exception
+    if s is None:
+        return None
     if not isinstance(s, str):
         raise TypeError('Not a string')
-
-    if s:
-        return ' '.join([s.split()[x].upper()
-                         if x % 2 == 0 else s.split()[x].lower()
-                         for x in range(len(s.split()))])
-    return None
+    return ' '.join(w.upper() if i % 2 == 0 else w.lower() for i, w in
+                    enumerate(s.split()))
 
 
 # Waypoint 11: Factorial
 def factorial(n):
     """Calculate factorial of a number (n!)."""
-    # Catch exception
     if not isinstance(n, int):
         raise TypeError('Not an integer')
     if n < 0:
@@ -121,7 +111,6 @@ def factorial(n):
 # Waypoint 12: Convert a Digit Character to an Integer
 def char_to_int(c):
     """Convert a digit character to an integer."""
-    # Catch exception
     if not isinstance(c, str):
         raise TypeError('Not a string')
     if len(c) > 1 or ord(c) < 48 or ord(c) > 57:
@@ -132,7 +121,6 @@ def char_to_int(c):
 # Waypoint 13: Convert a String of Digit Characters to an Integer
 def string_to_int(s):
     """Convert a string of digit characters to an integer."""
-    # Catch exception
     if not isinstance(s, str):
         raise TypeError('Not a string')
     integer = 0
@@ -147,13 +135,10 @@ def string_to_int(s):
 # Waypoint 14: Test Palindrome String
 def is_palindrome(value):
     """Check whether a string is palindrome."""
-    # Convert value to string
     # Remove punctuation
     changed_value = str(value).lower().translate(
-        str(value).maketrans("", "", string.punctuation))
-    # Remove whitespaces
+        str(value).maketrans("", "", punctuation))
     changed_value = changed_value.replace(' ', '')
-    # Compare first character with last character
     return changed_value == changed_value[::-1]
 
 
@@ -165,7 +150,6 @@ def roman_numeral_to_int(roman_numeral):
     subtraction = {"IV": -2, "IX": -2, "XL": -20, "XC": -20, "CD": -200,
                    "CM": -200}
     add = 0
-    # Catch exception
     if not isinstance(roman_numeral, str):
         raise TypeError('Not a string')
     if roman_numeral:
@@ -183,13 +167,13 @@ def roman_numeral_to_int(roman_numeral):
 
 # Waypoint 16: Play a Melody
 def play_melody(melody, sound_basedir):
-    """Play sound."""
-    pygame.init()
+    """Play sounds."""
+    init()
     notes = {'C#': 'Db', 'D#': 'Eb', 'F#': 'Gb', 'G#': 'Ab', 'A#': 'Bb'}
     for note in melody:
         if '#' in note:
             note = note.replace(note[:2], notes[note[:2]])
-        path = '{}{}.ogg'.format(sound_basedir, note.lower())
+        path = '{}/{}.ogg'.format(sound_basedir, note.lower())
         print(path)
-        pygame.mixer.Sound(path).play(maxtime=450, fade_ms=50)
-        time.sleep(0.2)
+        Sound(path).play(maxtime=450, fade_ms=50)
+        sleep(0.2)
